36c5bd153769976f2843e752e44a5f5174878b38
Ming <lei.min@northeastern.edu>
2026-04-21 17:59:16 -0400

    commit

--- src/main/java/edu/gui_mvc/view/JFrameView.java
+++ src/main/java/edu/gui_mvc/view/JFrameView.java
@@ -3,6 +3,8 @@ package edu.gui_mvc.view;
 import edu.gui_mvc.model.Features;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,13 +18,21 @@ public class JFrameView extends JFrame implements IView {
   public JFrameView(String title) {
     super(title);
     setLayout(new FlowLayout());
+    GridBagConstraints gbc = new GridBagConstraints();
 
     label = new JLabel("data is loading ...");
     upperBtn = new JButton("Upper Case");
     colorBtn = new JButton("Toggle Color");
-    add(label);
-    add(upperBtn);
-    add(colorBtn);
+
+    gbc.insets = new Insets(5, 5, 5, 5);
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    add(label,gbc);
+    gbc.gridx = 1;
+    add(upperBtn,gbc);
+
+    gbc.gridx = 2;
+    add(colorBtn, gbc);
 
     setSize(400, 200);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
